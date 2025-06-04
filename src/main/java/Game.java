42bb28eca7482b771c2ef081277106a67db121ea
sunw.moon <sunw.moon@samsung.com>
2025-06-04 13:50:30 +0900

    [feature] 1p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,11 @@
 public class Game {
+
+    String answer = "123";
+
+    public GuessResult guess(String number) {
+        if (answer.equals(number))
+            return GuessResult.of(3, 0);
+
+        throw new IllegalArgumentException();
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    @Test
+    void threeStrikeTest() {
+        Game game = new Game();
+
+        GuessResult ret = game.guess("123");
+
+        assertEquals(ret.getStrikes(), 3);
+        assertEquals(ret.getBalls(), 0);
+    }
 }
